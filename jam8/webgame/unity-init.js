eff98176a3010a937b06f9f200546eafce5c5260
d.starohorodskyi <d.starohorodskyi@plarium.com>
2026-08-22 02:29:32 +0200

    Update web build 0.1.22

--- jam8/webgame/unity-init.js
+++ jam8/webgame/unity-init.js
@@ -20,7 +20,7 @@
     streamingAssetsUrl: 'StreamingAssets',
     companyName: 'ItWorks!',
     productName: 'Devil’s in Detail',
-    productVersion: '0.1.21'
+    productVersion: '0.1.22'
   }, function(progress) {
     // feeds the thin bar under the Play button
     if (typeof window.webgameSetProgress === 'function') window.webgameSetProgress(progress);
